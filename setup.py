f9bf37b7d0cf6ded534c801573994379aad5032a
Máté Marjai <mate@theproduct.works>
2017-03-14 13:30:23 +0000

    Change the description on setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='worldpay',
       version='0.1',
-      description='Integrated example application using Worldpay Orders API',
+      description='Wrapper for Worldpay Orders API',
       url='https://github.com/TestingPays',
       author='Luke Rickard',
       author_email='luke.rickard@thepayment.works',
